8e398dd1fd5bf98a987a7038598b0411cef837b1
jakub_zika <kuba.zika@email.cz>
2017-04-13 02:11:39 +0200

    Add new dependencies and production script. Add production webpack config

---
+++ client/internals/webpack/webpack.prod.babel.js
@@ -0,0 +1,101 @@
+const webpack = require('webpack');
+const path = require('path');
+const fs = require('fs');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const cheerio = require('cheerio');
+
+
+const plugins = [
+  // new webpack.NoErrorsPlugin(),
+  new HtmlWebpackPlugin({
+    path: '',
+    inject: true,
+    templateContent: template(), //eslint-disable-line no-use-before-define
+  }),
+  new webpack.optimize.CommonsChunkPlugin({
+    name: 'vendor',
+    minChunks(module) {
+      // this assumes your vendor imports exist in the node_modules directory
+      return module.context && module.context.indexOf('node_modules') !== -1;
+    },
+  }),
+  // CommonChunksPlugin will now extract all the common modules from vendor and main bundles
+  new webpack.optimize.CommonsChunkPlugin({
+    name: 'manifest', // But since there are no more common modules between them we end up with just the runtime code included in the manifest file
+  }),
+];
+
+module.exports = {
+  entry: [
+    path.join(process.cwd(), 'app/app.js'),
+  ],
+  output: {
+    path: path.resolve(process.cwd(), 'build'),
+    publicPath: '',
+    filename: './[name].js',
+    chunkFilename: '[name].chunk.js',
+  },
+  devtool: 'inline-source-map',
+  plugins,
+  module: {
+    loaders: [{
+      test: /\.js?$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/,
+      query: {
+        // presets: ['babel-preset-react-hmre'].map(require.resolve),
+      },
+    },
+    {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader?modules', 'postcss-loader'],
+    },
+    {
+      test: /\.(eot|svg|ttf|woff|woff2)$/,
+      loader: 'file-loader',
+    }, {
+      test: /\.(jpg|png|gif)$/,
+      loaders: [
+        'file-loader',
+        {
+          loader: 'image-webpack-loader',
+          query: {
+            progressive: true,
+            optimizationLevel: 7,
+            interlaced: false,
+            pngquant: {
+              quality: '65-90',
+              speed: 4,
+            },
+          },
+        },
+      ],
+    }, {
+      test: /\.html$/,
+      loader: 'html-loader',
+    }, {
+      test: /\.json$/,
+      loader: 'json-loader',
+    }, {
+      test: /\.(mp4|webm)$/,
+      loader: 'url-loader',
+      query: {
+        limit: 10000,
+      },
+    },
+    ],
+  },
+  node: {
+    net: 'empty',
+    fs: 'empty',
+    tls: 'empty',
+    dns: 'empty',
+  },
+};
+
+function template() {
+  const html = fs.readFileSync(path.resolve(process.cwd(), 'app/index.html')).toString();
+  const document = cheerio(html);
+
+  return document.toString();
+}
