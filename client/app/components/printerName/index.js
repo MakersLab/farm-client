6056cdcba08e896ca17f08f4e5192fd2e791f3e4
jakub_zika <kuba.zika@email.cz>
2017-04-13 02:22:41 +0200

    Add missing utils library and printerName component

---
+++ client/app/components/printerName/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import style from '../printer/style.css';
+
+const PrinterName = text => (<div className={style.printerName}>{text}</div>);
+
+export default PrinterName;
