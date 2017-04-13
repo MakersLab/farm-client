18a8028ea574d0dbe77ea0f7892a3fe387ef989a
jakub_zika <kuba.zika@email.cz>
2017-04-13 02:27:05 +0200

    Define action constants for printer control

--- client/app/actions/mainView.js
+++ client/app/actions/mainView.js
@@ -1,8 +1,16 @@
 
 export const PRINTER_ADD = 'PRINTER_ADD';
 export const PRINTER_UPDATE_STATE = 'PRINTER_UPDATE_STATE';
+export const PRINTER_TOGGLE = 'PRINTER_TOGGLE';
 
-export const addPrinter = (printer, id) => ({
+export const PRINTER_PRINT = 'PRINTER_PRINT';
+export const PRINTER_TOGGLE_PAUSE = 'PRINTER_TOGGLE_PAUSE';
+export const PRINTER_LOAD = 'PRINTER_LOAD';
+export const PRINTER_STOP = 'PRINTER_STOP';
+
+export const PRINTER_REQUEST_COMPLETED = 'PRINTER_REQUEST_COMPLETED';
+
+export const addPrinter = (printer, id, link) => ({
   type: PRINTER_ADD,
   printer,
   id,
