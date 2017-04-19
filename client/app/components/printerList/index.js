64383553c01d3fac4105e2f39661032bc8c394bf
jakub_zika <kuba.zika@email.cz>
2017-04-19 19:45:34 +0200

    Display only two printers per row

--- client/app/components/printerList/index.js
+++ client/app/components/printerList/index.js
@@ -1,24 +1,38 @@
 import React, { PropTypes } from 'react';
 
+import style from './style.css';
 import Printer from '../printer';
 
 class PrinterListComponent extends React.Component {
-  constructor(props) {
-    super(props);
+
+  getPrinterComponent(key) {
+    return (<Printer
+      {...this.props.printers[key]} key={key}
+      toggleSelected={() => { this.props.toggleSelected(key); }} />);
   }
 
-  generatePrinterList(printers) {
-    return Object.keys(printers).map(printer =>
-      <Printer
-        {...printers[printer]} key={printer}
-        toggleSelected={() => { this.props.toggleSelected(printer); }} />,
-    );
+  generatePrinterList() {
+    let keys = Object.keys(this.props.printers);
+    let printerRows = [];
+    if (keys.length > 0) {
+      let howManyTimes = Math.floor(keys.length / 2);
+      for (let i = 0; i < howManyTimes; i+=1) {
+        console.log(Math.floor(keys.length/2));
+        printerRows.push(
+          <div key={i}>
+            {this.getPrinterComponent(keys[i * 2])}
+            { keys[(i * 2) + 1] && this.getPrinterComponent(keys[(i * 2) + 1])}
+          </div>);
+      }
+      return printerRows;
+    }
+    return (<div></div>);
   }
 
   render() {
     return (
-      <div>
-        {this.generatePrinterList(this.props.printers)}
+      <div className={style.printerList}>
+        {this.generatePrinterList()}
       </div>);
   }
 }
