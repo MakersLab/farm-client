3ae5bc1c944efc536bfda7bfdd6a0cd6efa05b60
Jakub Zika <kuba.zika@email.cz>
2017-05-27 14:22:35 +0200

    File input now accepts .gcode and .gco extensions

--- client/app/components/fileUploadModal/index.js
+++ client/app/components/fileUploadModal/index.js
@@ -41,7 +41,7 @@ const FileUploadModal = ({ isOpen, children, close, confirm, isUploadingFile, se
     <SelectedPrinters selectedPrinters={selectedPrinters} />
     <span className={style.fileName}>{fileInput && extractFileNameFromPath(fileInput.value)}</span>
     <ControllButton onClick={() => { fileInput.click(); }}>Load file</ControllButton>
-    <input type="file" hidden open ref={(input) => { fileInput = input; }} accept=".gco"/>
+    <input type="file" hidden open ref={(input) => { fileInput = input; }} accept=".gcode, .gco"/>
     <ControllButton disabled={confirmButtonstate()} onClick={() => { onUploadButtonClick(confirm); }}>Upload</ControllButton>
   </Modal>
 );
